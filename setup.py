2463bca8e9b9d23befd74256103972b29788027e
Ondřej Caletka <ondrej@caletka.cz>
2018-08-16 21:25:11 +0200

    Quirk file generator, rewritten quirks

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     entry_points={
         "console_scripts": [
             "spotzurnal = spotzurnal.main:main",
+            "spotzurnal-quirkgen = spotzurnal.quirkgen:quirkgen",
         ],
     },
 )
---
+++ spotzurnal/quirkgen.py
@@ -0,0 +1,56 @@
+import sys
+from pathlib import Path
+
+import click
+from yaml import safe_load
+
+from .cache import Cache
+
+
+@click.command()
+@click.option(
+    "--cache",
+    metavar="<cache_sqlite_file>",
+    show_default=True,
+    type=click.Path(dir_okay=False),
+    default=str(Path(click.get_app_dir("spotzurnal")) / "cache.sqlite"),
+    help="Path to SQLite cache.",
+)
+@click.option(
+    "--quirks",
+    metavar="<quirks_yaml_file>",
+    show_default=True,
+    type=click.File(),
+    help="File with existing quirks",
+)
+@click.option(
+    "--output",
+    metavar="<output_yaml_file>",
+    show_default=True,
+    type=click.File('w'),
+    default=sys.stdout,
+    help="Output file.",
+)
+def quirkgen(cache, quirks, output):
+    """
+    Get all unmatched songs from cache. Add them to quirks file for manual
+    resolution.
+    """
+    if quirks:
+        q = safe_load(quirks)
+    else:
+        q = {"artists": {}, "tracks": {}}
+    c = Cache(cache)
+    tracks = c.get_unmatched_tracks()
+    # We generate our custom YAML with coments
+    output.write("---\n\ntracks:\n")
+    for t in tracks:
+        quirk = q["tracks"].get(t.track_id, "")
+        output.write(
+            f"# {t.interpret} ({t.interpret_id}) - {t.track}\n"
+            f"  {t.track_id}: \"{quirk}\"\n",
+        )
+    output.write("\nartists:\n")
+    for k, v in q["artists"].items():
+        output.write(f"  {k}: \"{v}\"\n")
+    output.write("\n...\n")
