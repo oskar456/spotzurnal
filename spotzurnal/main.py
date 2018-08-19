d56b3722774ba82db23966569614f23dbc789a5d
Ondřej Caletka <ondrej@caletka.cz>
2018-08-19 13:26:00 +0200

    Refactor playlist matching logic out of main click function

--- spotzurnal/main.py
+++ spotzurnal/main.py
@@ -1,6 +1,4 @@
 import datetime
-import locale
-import re
 from pathlib import Path
 
 import click
@@ -41,38 +39,6 @@ class ClickDate(click.ParamType):
                 param,
                 ctx,
             )
-
-
-def get_plname(station, date):
-    """Return name of playlist for certain station and date."""
-    locale.setlocale(locale.LC_TIME, "cs_CZ")
-    mname = (
-        None, "ledna", "února", "března", "dubna", "května", "června",
-        "července", "srpna", "září", "října", "listopadu", "prosince",
-    )
-    return "{} {} {}. {} {}".format(
-        croapi.get_cro_station_name(station),
-        date.strftime("%A").lower(),
-        date.day,
-        mname[date.month],
-        date.year,
-    )
-
-
-def get_track_quirk(quirks, cro_track_id):
-    """Return Spotify track id for given CRo track id from quirks."""
-    q = quirks["tracks"].get(cro_track_id)
-    if q:
-        m = re.search(r"(?:track.)?([0-9a-zA-Z]{22})", q)
-        if m:
-            return m.group(1)
-
-
-def get_artist_quirk(quirks, interpret_id):
-    i = quirks["artists"].get(interpret_id)
-    if i:
-        click.secho(f"Corrected artist to {i}.", fg="yellow")
-        return i
 
 
 @click.command()
@@ -130,70 +96,5 @@ def main(credentials, username, date, station, replace, cache, quirks):
     if quirks:
         q = safe_load(quirks)
     else:
-        q = {"artists": {}, "tracks": {}}
-    spotracks = []
-    unmatched = []
-    fromcache = 0
-    pl = croapi.get_cro_day_playlist(station, date)
-    for n, track in enumerate(pl, start=1):
-        c.store_cro_track(track)
-        m = get_track_quirk(q, track.track_id) or c.lookup_match(track)
-        if m:
-            fromcache += 1
-            t = {"id": m}
-        else:
-            print(f"{track.since:%H:%M}: {track.interpret} - {track.track}")
-            interpret = (
-                get_artist_quirk(q, track.interpret_id) or track.interpret
-            )
-            t = matcher.search_spotify_track(sp, interpret, track.track)
-            if t:
-                c.store_spotify_track(t, track)
-        if t:
-            spotracks.append(t)
-        else:
-            unmatched.append(track)
-    matched = len(spotracks)
-    if matched < 1:
-        raise SystemExit(click.style("No tracks found!", fg="red"))
-    pct, cachepct = 100*matched/n, 100*fromcache/matched
-    click.secho(f"Matched {matched}/{n} – {pct:.0f}%", bold=True)
-    click.secho(
-        f"Already cached {fromcache}/{matched} – {cachepct:.0f}%",
-        bold=True,
-    )
-    if unmatched:
-        click.secho("Unmatched tracks:", bold=True)
-        print("\n".join(
-            f"{t.since:%H:%M}: {t.interpret} ({t.interpret_id}) - "
-            f"{t.track} ({t.track_id})"
-            for t in unmatched
-        ))
-    plname = get_plname(station, date)
-    print(plname)
-    playlist = sp.get_or_create_playlist(plname)
-    click.secho(
-        "Playlist URL: https://open.spotify.com/user/"
-        f"{sp.user}/playlist/{playlist}",
-        bold=True,
-    )
-    trackids = [t["id"] for t in spotracks]
-    if replace:
-        sp.user_playlist_replace_tracks(sp.user, playlist, trackids[:100])
-    total = sp.user_playlist_tracks(sp.user, playlist, fields="total")["total"]
-    if 0 < total < matched and not replace:
-        print(
-            "Keeping {} tracks already in playlist, adding {} more.".format(
-                total, matched - total,
-            ),
-        )
-    if total >= matched and not replace:
-        print("No new tracks found.")
-    else:
-        sp.put_all_data(
-            sp.user_playlist_add_tracks,
-            trackids,
-            sp.user,
-            playlist,
-            offset=total,
-        )
+        q = None
+    matcher.match_cro_playlist(sp, date, station, replace, c, q)
--- spotzurnal/matcher.py
+++ spotzurnal/matcher.py
@@ -1,7 +1,11 @@
-
+import re
+import locale
 from difflib import SequenceMatcher
 
 import click
+
+from . import croapi
+from .cache import Cache
 
 
 def get_spotify_artist_title(spotrack):
@@ -96,3 +100,115 @@ def search_spotify_track(sp, croartist, crotitle):
     print_spotify_track(items[n], fg="green")
     click.secho(f"^ Matched with {ara[n]:.2f}, {tra[n]:.2f}", fg="cyan")
     return items[n]
+
+
+def get_plname(station, date):
+    """Return name of playlist for certain station and date."""
+    locale.setlocale(locale.LC_TIME, "cs_CZ")
+    mname = (
+        None, "ledna", "února", "března", "dubna", "května", "června",
+        "července", "srpna", "září", "října", "listopadu", "prosince",
+    )
+    return "{} {} {}. {} {}".format(
+        croapi.get_cro_station_name(station),
+        date.strftime("%A").lower(),
+        date.day,
+        mname[date.month],
+        date.year,
+    )
+
+
+def get_track_quirk(quirks, cro_track_id):
+    """Return Spotify track id for given CRo track id from quirks."""
+    q = quirks["tracks"].get(cro_track_id)
+    if q:
+        m = re.search(r"(?:track.)?([0-9a-zA-Z]{22})", q)
+        if m:
+            return m.group(1)
+
+
+def get_artist_quirk(quirks, interpret_id):
+    i = quirks["artists"].get(interpret_id)
+    if i:
+        click.secho(f"Corrected artist to {i}.", fg="yellow")
+        return i
+
+
+def match_cro_playlist(
+        sp, date, station, replace=False, cache=None, quirks=None,
+):
+    """
+    Generate a Spotify playlist from a playlist published
+    by the Czech Radio.
+    """
+    c = cache or Cache()
+    q = quirks or {"artists": {}, "tracks": {}}
+    trackids = []
+    unmatched = []
+    fromcache = 0
+    pl = croapi.get_cro_day_playlist(station, date)
+    for n, track in enumerate(pl, start=1):
+        c.store_cro_track(track)
+        m = get_track_quirk(q, track.track_id) or c.lookup_match(track)
+        if m:
+            fromcache += 1
+        else:
+            print(f"{track.since:%H:%M}: {track.interpret} - {track.track}")
+            interpret = (
+                get_artist_quirk(q, track.interpret_id) or track.interpret
+            )
+            t = search_spotify_track(sp, interpret, track.track)
+            if t:
+                c.store_spotify_track(t, track)
+                m = t.get("id")
+        if m:
+            trackids.append(m)
+        else:
+            unmatched.append(track)
+    matched = len(trackids)
+    if matched < 1:
+        click.secho("No tracks found!", fg="red")
+        return
+    pct, cachepct = 100*matched/n, 100*fromcache/matched
+    click.secho(f"Matched {matched}/{n} – {pct:.0f}%", bold=True)
+    click.secho(
+        f"Already cached {fromcache}/{matched} – {cachepct:.0f}%",
+        bold=True,
+    )
+    if unmatched:
+        click.secho("Unmatched tracks:", bold=True)
+        print("\n".join(
+            f"{t.since:%H:%M}: {t.interpret} ({t.interpret_id}) - "
+            f"{t.track} ({t.track_id})"
+            for t in unmatched
+        ))
+    plname = get_plname(station, date)
+    click.secho(
+        f"Playlist name: {plname}",
+        bold=True,
+    )
+    playlist = sp.get_or_create_playlist(plname)
+    click.secho(
+        "Playlist URL: https://open.spotify.com/user/"
+        f"{sp.user}/playlist/{playlist}",
+        bold=True,
+    )
+    if replace:
+        sp.user_playlist_replace_tracks(sp.user, playlist, trackids[:100])
+    total = sp.user_playlist_tracks(sp.user, playlist, fields="total")["total"]
+    if 0 < total < matched and not replace:
+        print(
+            "Keeping {} tracks already in playlist, adding {} more.".format(
+                total, matched - total,
+            ),
+        )
+    if total >= matched and not replace:
+        print("No new tracks found.")
+    else:
+        sp.put_all_data(
+            sp.user_playlist_add_tracks,
+            trackids,
+            sp.user,
+            playlist,
+            offset=total,
+        )
